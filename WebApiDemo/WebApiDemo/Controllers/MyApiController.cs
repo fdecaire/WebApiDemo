6e4965c176b10148124389efd613b6d9fc488c22
fdecaire <frank@decaire.net>
2014-11-15 14:04:03 -0500

    Web API demonstration applications

---
+++ WebApiDemo/WebApiDemo/Controllers/MyApiController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+//http://www.franksdomain.com/WebApiDemo/api/MyApi
+namespace WebApiDemo.Controllers
+{
+	public class MyApiController : ApiController
+	{
+		[HttpPost]
+		[ActionName("GetInventory")]
+		public HttpResponseMessage GetInventory([FromBody] ApiRequest request)
+		{
+			if (request == null)
+			{
+				return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Request was null");
+			}
+			
+			// check authentication
+			var auth = ControllerContext.Request.Headers.Authorization;
+			
+			// simple demonstration of user rights checking.
+			if (auth.Scheme != "ABCD")
+			{
+				return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Invalid Credentials");
+			}
+		
+			ApiResponse apiResponse = new ApiResponse();
+
+			// read data from a database
+			apiResponse.Records = DummyDataRetriever.ReadData(request.ProductId);
+
+			// convert the data into json
+			var jsonData = JsonConvert.SerializeObject(apiResponse);
+
+			var resp = new HttpResponseMessage();
+			resp.Content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			return resp;
+		}
+	}
+}
---
+++ WebApiRetreiver/WebApiRetriever/WebApiRetriever.cs
@@ -0,0 +1,49 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Net;
+using log4net;
+using Newtonsoft.Json;
+using System.Text;
+using System;
+
+namespace WebApiRetreiver
+{
+	public class WebApiRetriever
+	{
+		private readonly string apiURLLocation = ConfigurationManager.AppSettings["ApiURLLocation"];
+		private readonly string apiAuthorization = ConfigurationManager.AppSettings["ApiCredential"];
+
+		public void Retreiver()
+		{
+			var serializer = new JsonSerializer();
+
+			var apiRequest = new ApiRequest
+			{
+				StoreId = 1,
+				ProductId = { 2, 3, 4 }
+			};
+
+			var request = (HttpWebRequest)WebRequest.Create(apiURLLocation + "");
+			request.ContentType = "application/json; charset=utf-8";
+			request.Accept = "application/xml";
+			request.Method = "POST";
+			request.Headers.Add(HttpRequestHeader.Authorization, apiAuthorization);
+			request.UserAgent = "ApiRequest";
+
+			//Writes the ApiRequest Json object to request 
+			using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+			{
+				streamWriter.Write(JsonConvert.SerializeObject(apiRequest));
+				streamWriter.Flush();
+			}
+
+			var httpResponse = (HttpWebResponse)request.GetResponse();
+
+			using (var streamreader = new StreamReader(httpResponse.GetResponseStream()))
+			using (var reader = new JsonTextReader(streamreader))
+			{
+				var storeInventory = serializer.Deserialize<ApiResponse>(reader);
+			}
+		}
+	}
+}
